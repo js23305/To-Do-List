ec18e12dbf96efc9eae8cf87706f888c536d13d8
js23305 <jasminesingla652@gmail.com>
2025-02-04 17:47:34 +0530

    updated models,py

--- todolist/members/models.py
+++ todolist/members/models.py
@@ -3,8 +3,6 @@ from django.db import models
 # Create your models here.
 class Task(models.Model):
     title = models.CharField(max_length=100)
-    decription = models.TextField(blank=True)
-    is_done = models.BooleanField(default=False)
-
+    completed = models.BooleanField(default=False)
     def __str__(self):
         return self.title
