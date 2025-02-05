c42fcc0e71da9058a25ac598fd0f2040b6da430d
js23305 <jasminesingla652@gmail.com>
2025-02-05 17:43:34 +0530

    removed code and added new code lines in html

--- to_do_list/members/views.py
+++ to_do_list/members/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from django.template import loader
 
 def members(request):
-    return HttpResponse("Hello world!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
